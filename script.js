240674c6f222c88478b65bdee394778b898f776a
Marc Sebastian Delas Alas <seb.archi0714@gmail.com>
2025-08-04 18:36:14 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function start() {
+    const choicesContainer = document.getElementById("choicesContainer");
+    const h1 = document.getElementById("playerText");
+    choicesContainer.style.display = "none";
+    h1.style.display = "none";
+}
